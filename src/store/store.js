a1e4267e5ca485a68d3c081f9f6e8356af31efa4
Prince <ranap8348@gmail.com>
2023-08-16 16:02:36 +0530

    modified store

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import homeSlice from "./homeSlice";
+import homeSlice from "./homeslice";
 export default configureStore({
   reducer: {
     home: homeSlice
